fa352e03a457d1b939ccbc0b578cd29915347b98
Dylan Dunn <dylandunn17@icloud.com>
2026-03-25 23:07:08 -0400

    Implemented Person Model

---
+++ Karen/Models/Person.swift
@@ -0,0 +1,27 @@
+//
+//  Person.swift
+//  Karen
+//
+//  Created by Dylan Dunn on 3/25/26.
+//
+
+import Foundation
+
+struct Person: Codable {
+    let id: UUID?
+    let firstname: String
+    let middlename: String
+    let lastname: String
+    
+    init (
+        id: UUID? = nil,
+        firstname: String,
+        middlename: String,
+        lastname: String
+    ) {
+        self.id = id
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
+    }
+}
