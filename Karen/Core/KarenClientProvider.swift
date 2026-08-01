528f49b4181ad844cc97f5066db3c35aca35bfdd
Dylan Dunn <dylandunn17@icloud.com>
2026-08-01 11:48:59 -0400

    Implemented API for direct access to Atlas, some mild file organization, undid codex's seperation of KarenClient and KarenKit

--- Karen/Core/KarenClientProvider.swift
+++ Karen/Core/KarenClientProvider.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import KarenClient
+import KarenKit
 
 enum KarenClientProvider {
     static let shared: KarenClient = {
--- Karen/Modules/Vehicles/ViewModels/VehicleDetailViewModel.swift
+++ Karen/Modules/Vehicles/ViewModels/VehicleDetailViewModel.swift
@@ -7,7 +7,7 @@
 
 import Combine
 import Foundation
-import KarenClient
+import KarenKit
 import KarenShared
 
 @MainActor
